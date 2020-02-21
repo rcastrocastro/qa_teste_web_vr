48fd8a1e5688641730d89f3c3beae1eefa305cb4
Raphael Castro <raphael_antonio_vieira@hotmail.com>
2020-02-21 12:14:29 -0300

    feat(testes-automatizados): add report_builder

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,25 @@
+require "report_builder"
+require "date"
+
 Before do
   @service = ->(klass) { klass.new }
+end
+
+at_exit do
+  date = Time.now.strftime("%d/%B/%Y")
+  time = Time.now.strftime("%HH%MM%SS")
+  @infos = {
+    "Data do Teste" => date,
+    "Hora do Teste" => time,
+  }
+
+  ReportBuilder.configure do |config|
+    config.input_path = "log/report.json"
+    config.report_path = "log/report"
+    config.report_types = [:html, :json]
+    config.report_title = "HTTParty"
+    config.additional_info = @infos
+    config.color = "indigo"
+  end
+  ReportBuilder.build_report
 end
